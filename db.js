c8e824231c6d1abcd32c0fefa2a267b2c1a0c473
Thomas Rory Gummerson <thomas@gummerson.no>
2017-07-26 19:06:28 +0200

    Fix for some URL-unfriendly chars

--- db.js
+++ db.js
@@ -7,7 +7,10 @@ const Datastore = require('nedb-promise');
 const hash = str =>
 	createHash('sha256')
 		.update(str)
-		.digest('base64');
+		.digest('base64')
+		.replace(/+/g, '-')
+		.replace(/\//g, '_')
+		.replace(/=/g, '');
 
 const createIDGenerator = db =>
 	function generate(str, len = 1) {
