74615b6ec6a62a13ec8c55be3a016ec276501984
Thomas Rory Gummerson <thomas@gummerson.no>
2017-07-31 10:36:41 +0200

    Fixes :)

--- web/app.js
+++ web/app.js
@@ -3,20 +3,18 @@
 const input = document.getElementById('input_url');
 const submit = document.getElementById('submit_url');
 const modal = document.getElementById('show_url');
+const urlTarget = document.getElementById('target_url');
 const close = document.getElementById('close_dialog');
 const copyBtn = new Clipboard('#copy_url');
 
 function showShortenedURL(value) {
-	const modalChild = modal.firstChild;
-	modalChild.innerHTML = `
-		${location.href + value}
-	`;
+	urlTarget.textContent = 
+	urlTarget.textContent = location.href + value;
 	modal.classList.add('visible');
 }
 
-function checkIfEmpty () {
-	const inputValue = input.value;
-	inputValue === ''
+function checkIfEmpty() {
+	return input.value === ''
 		? submit.disabled = true
 		: submit.disabled = false;
 }
