7bb9a68515be9b968da6ac6f0ae5eafb424fb771
Gitea <gitea@fake.local>
2017-09-26 14:50:22 +0200

    Absolute fixes

--- generateMarkup.js
+++ generateMarkup.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const { resolve } = require('path');
+const { join } = require('path');
 const { readFileSync, writeFileSync } = require('fs');
 
 const { compile } = require('pug');
 const { Parser, HtmlRenderer } = require('commonmark');
 
+const abs = path => join(__dirname, path);
+
 const markup = new HtmlRenderer().render(
 	new Parser().parse(
-		readFileSync(resolve('README.md'), 'utf8')));
+		readFileSync(abs('README.md'), 'utf8')));
 
-writeFileSync(resolve('web/index.html'),
-	compile(readFileSync('index.pug', 'utf8'), {
+writeFileSync(abs('web/index.html'),
+	compile(readFileSync(abs('index.pug'), 'utf8'), {
 		filters: {
 			commonmark() {
 				return markup;
